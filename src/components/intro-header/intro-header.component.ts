36bf79b96bf916f86b05e569a92b92693c2f69d0
Fred Zhang <zhangxu.fred@gmail.com>
2018-08-02 18:41:12 +0800

    new intro header component

--- src/components/intro-header/intro-header.component.ts
+++ src/components/intro-header/intro-header.component.ts
@@ -1,2 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
 import { introSlide } from '../../model/introslide'
 
+@Component({
+	selector: 'app-hero-detail',
+	templateUrl: './hero-detail.component.html',
+	styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+	@Input() hero: Hero;
+
+	constructor() {}
+
+	ngOnInit() {}
+
+}
