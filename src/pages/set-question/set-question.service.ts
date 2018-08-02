84ed1404583c818bc1935f4679faa651e67447e3
Fred Zhang <zhangxu.fred@gmail.com>
2018-08-02 18:33:20 +0800

    rm unused dependencies

--- src/pages/set-question/set-question.service.ts
+++ src/pages/set-question/set-question.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Request, RequestOptions, Response, RequestMethod, URLSearchParams } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
+import { Http, Headers, RequestOptions } from '@angular/http';
+// import { Observable } from 'rxjs/Observable';
+// import { Subject } from 'rxjs/Subject';
 
 import { Moment } from "../../model/moment";
 
@@ -14,7 +14,7 @@ export class SetQuestionService {
 
     public save(moment: Moment){
         console.log(moment);
-        
+
         //向后台post数据的写法如下
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
@@ -22,7 +22,7 @@ export class SetQuestionService {
         let data = JSON.stringify(moment);
 
         return this.http.post(this.userRegisterURL, data, options);
-        
+
     }
 
 }
