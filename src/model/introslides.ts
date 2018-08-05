b3e4811e47f181a2f0c3e43144074e808b74e4c3
Fred Zhang <zhangxu.fred@gmail.com>
2018-08-05 15:52:14 +0800

    update

---
+++ src/model/introslides.ts
@@ -0,0 +1,6 @@
+import { IntroSlide } from '../model/introslide';
+
+export const INTROSLIDES: IntroSlide[] = [
+	new IntroSlide('Treasure your love moments', 'Easily edit and store your love messages in a digital love box. ', 'assets/img/intro1.png', false),
+	new IntroSlide('Know more about him/her', 'Each message should be “locked” with a question and the question need to be answered before reading the message. ', 'assets/img/intro2.png', true),
+];
--- src/pages/intro/intro.service.ts
+++ src/pages/intro/intro.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { introSlide } from '../../model/introslide';
+import { INTROSLIDES } from '../../model/introslides';
+import { introSlide } from '../../model/introslide'
 import { Observable, of } from 'rxjs';
 
 @Injectable()
@@ -7,13 +8,8 @@ export class IntroSlideService {
 
 	constructor() {}
 
-	getSlides() {
-		return [
-
-		]
+	getSlides(): Observable<introSlide[]> {
+		return of(INTROSLIDES)
 	}
-
-	// new introSlide('Treasure your love moments', 'Easily edit and store your love messages in a digital love box. ', 'assets/img/intro1.png', false),
-	// new introSlide('Know more about him/her', 'Each message should be “locked” with a question and the question need to be answered before reading the message. ', 'assets/img/intro2.png', true),
 
 }
