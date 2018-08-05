5572453de69e217d2756fcce0a357d35c6bd3de4
Fred Zhang <zhangxu.fred@gmail.com>
2018-08-05 15:46:34 +0800

    update

--- src/pages/intro/intro.service.ts
+++ src/pages/intro/intro.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { introSlide } from '../../model/introslide';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class IntroSlideService {
@@ -8,9 +9,11 @@ export class IntroSlideService {
 
 	getSlides() {
 		return [
-			new introSlide('Treasure your love moments', 'Easily edit and store your love messages in a digital love box. ', 'assets/img/intro1.png', false),
-			new introSlide('Know more about him/her', 'Each message should be “locked” with a question and the question need to be answered before reading the message. ', 'assets/img/intro2.png', true),
+
 		]
 	}
+
+	// new introSlide('Treasure your love moments', 'Easily edit and store your love messages in a digital love box. ', 'assets/img/intro1.png', false),
+	// new introSlide('Know more about him/her', 'Each message should be “locked” with a question and the question need to be answered before reading the message. ', 'assets/img/intro2.png', true),
 
 }
