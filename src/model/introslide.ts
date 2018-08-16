2c66c7c34bf9d54ebabed84a57b7ce241a089423
Fred Zhang <zhangxu.fred@gmail.com>
2018-08-16 16:30:39 +0800

    intro页重构

---
+++ src/model/introslide.ts
@@ -0,0 +1,8 @@
+export class introSlide {
+	constructor(
+		public title: string,
+		public subtitle: string,
+		public imgSrc: string,
+		public isShowBtn: boolean,
+	) { }
+  }
--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,29 +1,29 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { NewOrNotPage } from "../new-or-not/new-or-not";
+import { introSlide } from '../../model/introslide';
 
-/**
- * Generated class for the IntroPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
-  selector: 'page-intro',
-  templateUrl: 'intro.html',
+	selector: 'page-intro',
+	templateUrl: 'intro.html',
 })
 export class IntroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	slides = [
+		new introSlide('Treasure your love moments', 'Easily edit and store your love messages in a digital love box. ', 'assets/img/intro1.png', false),
+		new introSlide('Know more about him/her', 'Each message should be “locked” with a question and the question need to be answered before reading the message. ', 'assets/img/intro2.png', true),
+	];
 
-  toNeworNotPage() {
-      this.navCtrl.push(NewOrNotPage);
-  }
+	constructor(public navCtrl: NavController, public navParams: NavParams)	{
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad IntroPage');
-  }
+	toNeworNotPage() {
+		this.navCtrl.push(NewOrNotPage);
+	}
+
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad IntroPage');
+	}
 
 }
